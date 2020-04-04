dc8f74ea1b80cd7eb19c30fc95f67cf73172686f
Colby Fayock <fayock@gmail.com>
2020-04-04 13:22:18 -0400

    setting up a basic map with a tilelayer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Map, TileLayer } from 'react-leaflet'
 import './App.css';
+import 'leaflet/dist/leaflet.css';
+
+const position = [ 38.9072, -77.0369 ];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Map center={position} zoom={8}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+        />
+      </Map>
     </div>
   );
 }
